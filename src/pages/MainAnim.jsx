e0b581f0fdd0491f5eb20d8644f72467bf7db561
amitdwivedi01 <shyamdwivedi595@gmail.com>
2023-10-15 18:05:56 +0530

    host updated

--- src/pages/MainAnim.jsx
+++ src/pages/MainAnim.jsx
@@ -1,61 +1,56 @@
 import React, { useState, useEffect } from "react";
 import Styles from "./MainAnim.module.css";
-import axios from 'axios';
+import axios from "axios";
 
 const MainAnim = () => {
   const [imgslist, setImgsList] = useState([]);
-  // let [imageCounter, setImageCounter] = useState(0)
-
 
   const fetchImages = async () => {
-    try{
-      const url =
-        "https://static.toiimg.com/thumb/msid-98627667,width-1280,resizemode-4/98627667.jpg";
-      
-            const newImage = {
-              url: url,
-              top:
-                Math.floor(Math.random() * (window.innerHeight -200)) + "px",
-              left:
-                Math.floor(Math.random() * (window.innerWidth - 200)) + "px",
-              randomn: Math.floor(Math.random() * (200 - 70 + 1)) + 70
-            };
-            console.log(newImage, );
-      setImgsList((prev) => [...prev, newImage]);
-      // const response = await axios.get("backend ka endpoint");
-      // if(response.status === 200){
-      //  const newImage = {
-            //   url: response ka url,
-            //   top:
-            //     Math.floor(Math.random() * (window.innerHeight -200)) + "px",
-            //   left:
-            //     Math.floor(Math.random() * (window.innerWidth - 200)) + "px",
-            //   randomn: Math.floor(Math.random() * (200 - 70 + 1)) + 70
-            // };
-      //   setImgsList((prev)=> [...prev, newImage])
-      // }
-    }catch(error){
-        console.log(error)
+    try {
+      const response = await axios.get("https://rich-cyan-cobra-wrap.cyclic.app/data");
+      console.log(response, "get img response");
+      if (
+        response.status === 200 &&
+        response.data.message != "No data available."
+      ) {
+        const newImage = {
+          url: response.data.imageUrl,
+          top: Math.floor(Math.random() * (window.innerHeight - 200)) + "px",
+          left: Math.floor(Math.random() * (window.innerWidth - 200)) + "px",
+          randomn: Math.floor(Math.random() * (200 - 70 + 1)) + 70,
+        };
+        setImgsList((prev) => [...prev, newImage]);
       }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    const intervalvar = setInterval(fetchImages, 1000);
+    const intervalvar = setInterval(fetchImages, 5000);
 
+    // cleanup function
     return () => clearInterval(intervalvar);
   }, []);
 
   return (
     <div className={`${Styles.mainanim_container}`}>
-      {imgslist.map((item, index) => (
-        <img
-          key={index}
-          className={`${Styles.image}`}
-          src={item.url}
-          alt=""
-          style={{ top: item.top, left: item.left, width: item.randomn, height: item.randomn }}
-        />
-      ))}
+      {imgslist.length > 0
+        ? imgslist.map((item, index) => (
+            <img
+              key={index}
+              className={`${Styles.image}`}
+              src={item.url}
+              alt=""
+              style={{
+                top: item.top,
+                left: item.left,
+                width: item.randomn,
+                height: item.randomn,
+              }}
+            />
+          ))
+        : ""}
     </div>
   );
 };
